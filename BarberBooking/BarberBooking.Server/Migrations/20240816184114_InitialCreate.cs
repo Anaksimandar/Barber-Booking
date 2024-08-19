0373bb2ee9e2fc36ffc94a7df5deab0afd847669
Aleksandar <acodex00@gmail.com>
2024-08-19 19:50:56 +0200

    Added migrations folder

---
+++ BarberBooking/BarberBooking.Server/Migrations/20240816184114_InitialCreate.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberBooking.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Service_Reservations_ReservationId",
+                table: "Service");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Service",
+                table: "Service");
+
+            migrationBuilder.RenameTable(
+                name: "Service",
+                newName: "Services");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Service_ReservationId",
+                table: "Services",
+                newName: "IX_Services_ReservationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Services",
+                table: "Services",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Services_Reservations_ReservationId",
+                table: "Services",
+                column: "ReservationId",
+                principalTable: "Reservations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Services_Reservations_ReservationId",
+                table: "Services");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Services",
+                table: "Services");
+
+            migrationBuilder.RenameTable(
+                name: "Services",
+                newName: "Service");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Services_ReservationId",
+                table: "Service",
+                newName: "IX_Service_ReservationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Service",
+                table: "Service",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Service_Reservations_ReservationId",
+                table: "Service",
+                column: "ReservationId",
+                principalTable: "Reservations",
+                principalColumn: "Id");
+        }
+    }
+}

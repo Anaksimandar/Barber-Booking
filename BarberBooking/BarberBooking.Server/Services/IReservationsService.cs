509abf2b61757d8e5607bfee4474abdc84a85c7a
Aleksandar <acodex00@gmail.com>
2024-08-19 19:45:06 +0200

    Added services for seeding data and working with reservations.

---
+++ BarberBooking/BarberBooking.Server/Services/IReservationsService.cs
@@ -0,0 +1,9 @@
+﻿using BarberBooking.Server.Entities;
+
+namespace BarberBooking.Server.Services
+{
+    public interface IReservationsService
+    {
+        Task<List<Reservation>> GetReservations();
+    }
+}
---
+++ BarberBooking/BarberBooking.Server/Services/ReservationService.cs
@@ -0,0 +1,21 @@
+﻿using BarberBooking.Server.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberBooking.Server.Services
+{
+    public class ReservationService : IReservationsService
+    {
+        private readonly BarberBookingContext _db;
+
+        public ReservationService(BarberBookingContext db)
+        {
+            _db = db;
+        }
+        public async Task<List<Reservation>> GetReservations()
+        {
+            var reservations = await _db.Reservations.ToListAsync();
+
+            return reservations;
+        }
+    }
+}

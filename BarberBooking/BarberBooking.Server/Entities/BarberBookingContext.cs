3933ac26ea02899f6a6d7d75ecb1d688613ba7d5
Aleksandar <acodex00@gmail.com>
2024-08-19 19:44:11 +0200

    Added entities/tables

---
+++ BarberBooking/BarberBooking.Server/Entities/BarberBookingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BarberBooking.Server.Entities
+{
+    public class BarberBookingContext : DbContext
+    {
+        public BarberBookingContext(DbContextOptions options):base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+    }
+}
---
+++ BarberBooking/BarberBooking.Server/Entities/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace BarberBooking.Server.Entities
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        
+    }
+}

ff42695e22e4f3436a827986d07b17d263ecb813
Aleksandar <acodex00@gmail.com>
2024-09-26 21:16:29 +0200

    added ListReservationsComponent for showing reservations for specific user or all reservations for admin

---
+++ BarberBooking/barberbooking.client/src/app/modules/list-reservations/list-reservations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-reservations',
+  templateUrl: './list-reservations.component.html',
+  styleUrls: ['./list-reservations.component.css']
+})
+export class ListReservationsComponent {
+
+}

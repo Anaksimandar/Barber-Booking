9b073c7d6af49b09ecc21ba16eca2863643b1b00
Aleksandar <acodex00@gmail.com>
2024-09-27 19:42:54 +0200

    added styled table with list of all reservations

--- BarberBooking/barberbooking.client/src/app/modules/list-reservations/list-reservations.component.ts
+++ BarberBooking/barberbooking.client/src/app/modules/list-reservations/list-reservations.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../../../models/reservation.model';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-list-reservations',
   templateUrl: './list-reservations.component.html',
   styleUrls: ['./list-reservations.component.css']
 })
-export class ListReservationsComponent {
+export class ListReservationsComponent implements OnInit {
+  public allReservations: Reservation[];
+
+  constructor(private httpClient:HttpClient, private notification:ToastrService) {
+    this.allReservations = [];
+  }
+  ngOnInit() {
+    this.getAllReservations();
+  }
+
+  getAllReservations() {
+    this.httpClient.get<Reservation[]>("https://localhost:7030/api/reservation").subscribe(
+      result => {
+        this.allReservations = result;
+      },
+      error => {
+        this.notification.error(error.message);
+      }
+    )
+  }
 
 }
